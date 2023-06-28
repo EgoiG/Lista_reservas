56818557c0081f3e754cfea6a65d7fa4cb157ef3
EgoiG <135364616+EgoiG@users.noreply.github.com>
2023-06-28 12:52:53 +0200

    Add files via upload

---
+++ ListaReservas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservasMVC.Models
+{
+    public class ListaReservas
+    {
+        private List<Reserva> _reserva;
+
+        /*public void PruebaListas()
+        {
+            _reserva = new List<Reserva>();
+            foreach(var reserva in _reserva)
+            {
+                Console.WriteLine("Id: " + reserva.IdReserva);
+            }
+        }*/
+
+        public void AniadirReserva()
+        {
+            _reserva.Add(new Reserva(1, DateTime.Parse("2023/01/22"), DateTime.Parse("2023/01/23"), 2000, 6, 1));
+            _reserva.Add(new Reserva(2, DateTime.Parse("2023/01/25"), DateTime.Parse("2023/01/26"), 1300, 7, 3));
+
+            foreach (var reserva in _reserva)
+            {
+                Console.WriteLine(reserva);
+            }
+        }
+
+        public void BorrarReserva()
+        {
+            _reserva.Remove(_reserva[0]);
+        }
+    }
+}
---
+++ Reserva.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservasMVC.Models
+{
+    class Reserva
+    {
+        private int _idReserva;
+        private DateTime _fechaEntrada;
+        private DateTime _fechaSalida;
+        private int _precio;
+        private int _idCliente;
+        private int _idAlojamiento;
+
+
+        //constructor
+        public Reserva()
+        {
+            _idReserva = 0;
+            _fechaEntrada = DateTime.MinValue;
+            _fechaSalida = DateTime.MinValue;
+            _precio = 0;
+            _idCliente = 0;
+            _idAlojamiento = 0;
+
+        }
+
+        public Reserva(int idReserva, DateTime fechaEntrada, DateTime fechaSalida, int idPrecio, int idCliente, int idAlojamiento)
+        {
+            _idReserva = idReserva;
+            _fechaEntrada = fechaEntrada;
+            _fechaSalida = fechaSalida;
+            _precio = idPrecio;
+            _idCliente = idCliente;
+            _idAlojamiento = idAlojamiento;
+        }
+
+        //propiedades
+        public int IdReserva { get => _idReserva; set => _idReserva = value; }
+        public DateTime FechaEntrada { get => _fechaEntrada; set => _fechaEntrada = value; }
+        public DateTime FechaSalida { get => _fechaSalida; set => _fechaSalida = value; }
+        public int Precio { get => _precio; set => _precio = value; }
+        public int IdCliente { get => _idCliente; set => _idCliente = value; }
+        public int IdAlojamiento { get => _idAlojamiento; set => _idAlojamiento = value; }
+
+        //metodos
+        public override string ToString()//el metodo ToString ya existe, hay que usar override para sobreescribirlo
+        {
+            return "idReserva: " + _idReserva + " Fecha de entrada: " + _fechaEntrada + " Fecha de salida: " + _fechaSalida + " Precio: " + _precio + " idCliente: " + _idCliente + "idAlojamiento: " + IdAlojamiento;
+        }
+    }
+}
